1ee8181275305a542b8ec449d05c88f42ae8368c
Tyler Kvochick <tyler@1build.com>
2020-04-02 15:29:25 -0700

    Remove tyler_functions dependency

--- AEGeAN_run.py
+++ AEGeAN_run.py
@@ -17,7 +17,7 @@ import torchvision.transforms as transforms
 from AEGeAN_1024 import GAN_D, GAN_G
 from torch.autograd import Variable
 from torch.backends import cudnn
-from tyler_functions import get_date_time
+import time
 cudnn.benchmark = True
 
 
@@ -32,9 +32,10 @@ def init_weights(m):
 
 to_image = transforms.ToPILImage()
 
-date, time = get_date_time()
+date = time.strftime('%y-%m-%d')
+time = time.strftime('%H-%M-%S')
 
-#these settings push a gtx1080 to the limit...
+# these settings push a gtx1080 to the limit...
 nf = 8
 n_iter = 1000
 z_d = 100
@@ -45,8 +46,9 @@ num_checkpoints = 7
 
 print('will save every {} epochs'.format(n_iter // num_checkpoints))
 
-dataroot = #INSERT DATA DIRECTORY HERE
-base_dir = '.../modelsAEGeAN1024_{}_{}/'.format(dataroot.split('/')[-1], date)  # Useful for making many models
+dataroot =  # INSERT DATA DIRECTORY HERE
+# Useful for making many models
+base_dir = '.../modelsAEGeAN1024_{}_{}/'.format(dataroot.split('/')[-1], date)
 checkpoint_path = base_dir + 'models/'
 save_dir = base_dir + 'images/'
 
@@ -59,21 +61,22 @@ dataset = dset.ImageFolder(root=dataroot,
                            transform=transforms.Compose([
                                transforms.Scale(image_size),
                                transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               transforms.Normalize(
+                                   (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                          shuffle=True, num_workers=int(workers))
 
 
-#---------------
-#Nets
-#---------------
+# ---------------
+# Nets
+# ---------------
 
 _Encoder = GAN_D(3, nf, nout=z_d)
 _Decoder = GAN_G(z_d, nf, nc=3)
 _Discriminator = GAN_D(3, nf, nout=1)
 
-#Replace final activation to match DCGAN
+# Replace final activation to match DCGAN
 _Discriminator.layers['act_9'] = nn.Sigmoid()
 
 _Encoder.build()
@@ -98,35 +101,36 @@ nets = [_Encoder, _Decoder, _Discriminator]
 
 [print(net.n_params()[0]) for net in nets]
 
-#---------------
-#Variables
-#---------------
+# ---------------
+# Variables
+# ---------------
 
 noise = torch.FloatTensor(batch_size, z_d, 1, 1)
 
-#Tried using a uniform distribution to allow the model to maintain more points in latent space. Didn't really work.
+# Tried using a uniform distribution to allow the model to maintain more points in latent space. Didn't really work.
 fixed_noise = torch.FloatTensor(batch_size, z_d, 1, 1).uniform_(-10, 10)
 torch.save(fixed_noise, base_dir + 'fixed_noise.pth')
 
 noise = Variable(noise).cuda()
 fixed_noise = Variable(fixed_noise).cuda()
 
-#---------------
-#Loss/Optimizers
-#---------------
+# ---------------
+# Loss/Optimizers
+# ---------------
 
 criterion = nn.BCELoss(size_average=True).cuda()
 
 optim_encoder = optim.SGD(_Encoder.parameters(), lr=1e-3, momentum=0.9)
 optim_decoder = optim.SGD(_Decoder.parameters(), lr=1e-3, momentum=0.9)
 optim_decoderGAN = optim.Adam(_Decoder.parameters(), lr=2e-4)
-optim_discriminator = optim.SGD(_Discriminator.parameters(), lr=2e-4, momentum=0.9)
+optim_discriminator = optim.SGD(
+    _Discriminator.parameters(), lr=2e-4, momentum=0.9)
 
-#these can be noised/annealed if needed
+# these can be noised/annealed if needed
 real_label = 1.0
 fake_label = 0.0
 
-#add explicit epochs to save
+# add explicit epochs to save
 save_list = [30, 60, 90]
 
 n_image_rows = int(math.sqrt(batch_size))
@@ -137,9 +141,9 @@ for epoch in range(n_iter):
 
         current_batch_size = batch.size(0)
 
-        #---------------
-        #Train as AE
-        #---------------
+        # ---------------
+        # Train as AE
+        # ---------------
 
         input = Variable(batch).cuda()
 
@@ -155,13 +159,13 @@ for epoch in range(n_iter):
         optim_decoder.step()
         _Decoder.zero_grad()
 
-        #---------------
-        #Train as GAN
-        #---------------
+        # ---------------
+        # Train as GAN
+        # ---------------
 
-        #Train Discriminator on real
+        # Train Discriminator on real
 
-        #_Discriminator.zero_grad()
+        # _Discriminator.zero_grad()
         real_samples = input.clone()
         inference_real = _Discriminator(real_samples)
         labels = torch.FloatTensor(current_batch_size).fill_(real_label)
@@ -169,14 +173,14 @@ for epoch in range(n_iter):
         real_loss = criterion(inference_real, labels)
         real_loss.backward()
 
-        #Generate fake samples
+        # Generate fake samples
 
         noise.data.resize_(current_batch_size, z_d, 1, 1)
         noise.data.uniform_(-10, 10)
         #noise.data.normal_(0, 1)
         fake_samples = _Decoder(noise, mode='gen')
 
-        #Train Discriminator on fake
+        # Train Discriminator on fake
 
         labels.data.fill_(fake_label)
         inference_fake = _Discriminator(fake_samples.detach())
@@ -185,9 +189,9 @@ for epoch in range(n_iter):
         discriminator_total_loss = real_loss + fake_loss
         optim_discriminator.step()
 
-        #Update Decoder/Generator with how well it fooled Discriminator
+        # Update Decoder/Generator with how well it fooled Discriminator
 
-        #_Decoder.zero_grad()
+        # _Decoder.zero_grad()
         labels.data.fill_(real_label)
         inference_fake_Decoder = _Discriminator(fake_samples)
         fake_samples_loss = criterion(inference_fake_Decoder, labels)
@@ -198,25 +202,37 @@ for epoch in range(n_iter):
             epoch + 1, n_iter, i + 1, len(dataloader), reconstruction_loss.data[0], discriminator_total_loss.data[0], fake_samples_loss.data[0])
         )
         if i % 35 == 0:
-            real_grid = vutils.make_grid(batch, n_image_rows, 0, normalize=True)
+            real_grid = vutils.make_grid(
+                batch, n_image_rows, 0, normalize=True)
             real_grid = to_image(real_grid)
             real_grid.save('{}/real_samples.gif'.format(save_dir))
 
-            decoded_grid = vutils.make_grid(reconstructed.data.clone().cpu(), n_image_rows, 0, normalize=True)
+            decoded_grid = vutils.make_grid(
+                reconstructed.data.clone().cpu(), n_image_rows, 0, normalize=True)
             decoded_grid = to_image(decoded_grid)
-            decoded_grid.save('{}/decoded_samples_epoch_{}_{}.gif'.format(save_dir, epoch, i))
+            decoded_grid.save(
+                '{}/decoded_samples_epoch_{}_{}.gif'.format(save_dir, epoch, i))
 
             fake = _Decoder(fixed_noise, mode='gen')
-            fake_grid = vutils.make_grid(fake.data.clone().cpu(), n_image_rows, 0, normalize=True)
+            fake_grid = vutils.make_grid(
+                fake.data.clone().cpu(), n_image_rows, 0, normalize=True)
             fake_grid = to_image(fake_grid)
-            fake_grid.save('{}/fake_samples_epoch_{}_{}.gif'.format(save_dir, epoch, i))
+            fake_grid.save(
+                '{}/fake_samples_epoch_{}_{}.gif'.format(save_dir, epoch, i))
 
-    if epoch % (n_iter // num_checkpoints) == 0 and epoch is not 0 or epoch in save_list:  # save n checkpoints (with optional list)
-        torch.save(_Encoder.state_dict(), '{}Encoder_nf{}_e{}_recon-err{:.4f}.pth'.format(checkpoint_path, nf, epoch, reconstruction_loss.data[0]))
-        torch.save(_Decoder.state_dict(), '{}Decoder_nf{}_e{}_fake-err{:.4f}.pth'.format(checkpoint_path, nf, epoch, fake_samples_loss.data[0]))
-        torch.save(_Discriminator.state_dict(), '{}Discriminator_nf{}_e{}_discrim-err{:.4f}.pth'.format(checkpoint_path, nf, epoch, discriminator_total_loss.data[0]))
+    # save n checkpoints (with optional list)
+    if epoch % (n_iter // num_checkpoints) == 0 and epoch is not 0 or epoch in save_list:
+        torch.save(_Encoder.state_dict(), '{}Encoder_nf{}_e{}_recon-err{:.4f}.pth'.format(
+            checkpoint_path, nf, epoch, reconstruction_loss.data[0]))
+        torch.save(_Decoder.state_dict(), '{}Decoder_nf{}_e{}_fake-err{:.4f}.pth'.format(
+            checkpoint_path, nf, epoch, fake_samples_loss.data[0]))
+        torch.save(_Discriminator.state_dict(), '{}Discriminator_nf{}_e{}_discrim-err{:.4f}.pth'.format(
+            checkpoint_path, nf, epoch, discriminator_total_loss.data[0]))
 
-#Final save just in case!
-torch.save(_Encoder.state_dict(), '{}Encoder_nf{}_e{}_recon-err{:.4f}.pth'.format(checkpoint_path, nf, epoch, reconstruction_loss.data[0]))
-torch.save(_Decoder.state_dict(), '{}Decoder_nf{}_e{}_fake-err{:.4f}.pth'.format(checkpoint_path, nf, epoch, fake_samples_loss.data[0]))
-torch.save(_Discriminator.state_dict(), '{}Discriminator_nf{}_e{}_discrim-err{:.4f}.pth'.format(checkpoint_path, nf, epoch, discriminator_total_loss.data[0]))
+# Final save just in case!
+torch.save(_Encoder.state_dict(), '{}Encoder_nf{}_e{}_recon-err{:.4f}.pth'.format(
+    checkpoint_path, nf, epoch, reconstruction_loss.data[0]))
+torch.save(_Decoder.state_dict(), '{}Decoder_nf{}_e{}_fake-err{:.4f}.pth'.format(
+    checkpoint_path, nf, epoch, fake_samples_loss.data[0]))
+torch.save(_Discriminator.state_dict(), '{}Discriminator_nf{}_e{}_discrim-err{:.4f}.pth'.format(
+    checkpoint_path, nf, epoch, discriminator_total_loss.data[0]))
